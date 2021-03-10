4f8857358f802b2716c113c720a0c843be8de8a3
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-03-10 16:29:36 -0600

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Craig Booker on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
